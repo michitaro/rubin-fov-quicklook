bb07fd84809e14098257b1a0bebdf4121b80823f
michitaro <michitaro.nike@gmail.com>
2024-11-28 14:21:57 +0900

    bugfix

--- backend/src/quicklook/utils/fitsheader.py
+++ backend/src/quicklook/utils/fitsheader.py
@@ -5,8 +5,8 @@ from quicklook.types import HeaderType
 
 def fitsheader_to_list(hdul: pyfits.HDUList) -> list[HeaderType]:
     headers: list[HeaderType] = []
-    cards: HeaderType = []
     for hdu in hdul:
+        cards: HeaderType = []
         for card in hdu.header.cards:  # type: ignore
             keyword, value, comment = card
             cards.append((keyword, value.__class__.__name__, stringify(value), comment))
